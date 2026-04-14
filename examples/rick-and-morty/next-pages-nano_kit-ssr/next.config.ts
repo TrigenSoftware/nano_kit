54e671bb212bce7c0df0e4fc082bee1a6244d982
dangreen <danon0404@gmail.com>
2026-04-14 18:49:04 +0000

    docs: fix nextjs examples

--- examples/rick-and-morty/next-pages-nano_kit-ssr/next.config.ts
+++ examples/rick-and-morty/next-pages-nano_kit-ssr/next.config.ts
@@ -1,24 +1,26 @@
 import type { NextConfig } from 'next'
 
-const nextConfig: NextConfig = {
-  transpilePackages: [
-    'agera',
-    'kida',
-    '@nano_kit/store',
-    '@nano_kit/router',
-    '@nano_kit/react',
-    '@nano_kit/react-router',
-    '@nano_kit/next-router',
-    '@nano_kit/query'
-  ],
-  webpack(config) {
-    config.resolve.extensionAlias = {
-      '.js': ['.ts', '.js'],
-      '.jsx': ['.tsx', '.jsx']
+const nextConfig: NextConfig = process.env.NANO_KIT_DEV
+  ? {
+    transpilePackages: [
+      'agera',
+      'kida',
+      '@nano_kit/store',
+      '@nano_kit/router',
+      '@nano_kit/react',
+      '@nano_kit/react-router',
+      '@nano_kit/next-router',
+      '@nano_kit/query'
+    ],
+    webpack(config) {
+      config.resolve.extensionAlias = {
+        '.js': ['.ts', '.js'],
+        '.jsx': ['.tsx', '.jsx']
+      }
+      config.optimization.minimize = false
+      return config
     }
-    // config.optimization.minimize = false
-    return config
   }
-}
+  : {}
 
 export default nextConfig
